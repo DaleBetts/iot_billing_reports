099fe675c147b39fab56bc0dc5926d2169dc4ba0
dale.betts <dale.betts@ubertasconsulting.com>
2018-04-09 09:34:56 +0100

    s3 file lookup code added

--- lambda/src/s3_billing_report/iot_billing_report.py
+++ lambda/src/s3_billing_report/iot_billing_report.py
@@ -4,6 +4,8 @@ import botocore
 import datetime
 import re
 import collections
+import csv
+import urllib2
 
 config = ConfigParser.RawConfigParser()
 config.read('./vars.ini')
@@ -11,9 +13,71 @@ config.read('./vars.ini')
 print('Loading IOT Billing Report Function')
 
 def lambda_handler(event, context):
-    regionsStrg = config.get('regions', 'regionList')
-    regionsList = regionsStrg.split(',')
-    EC2_INSTANCE_TAG = config.get('s3', 'reportBucket')
-    ec2_instance_tags = EC2_INSTANCE_TAG.split(',')
+    regions_strg = config.get('regions', 'region_list')
+    regions_list = regions_strg.split(',')
+    s3_bucket = config.get('s3', 'report_bucket')
+    custom_prefix = config.get('config', 'custom_prefix')
+    custom_suffix = config.get('config', 'custom_suffix')
+    custom_prefix.strip()
+    custom_suffix.strip()
+
+    for key in get_matching_s3_keys(bucket=s3_bucket,prefix=custom_prefix, suffix=custom_suffix):
+        print(key)
+
+def get_matching_s3_objects(bucket, prefix='', suffix=''):
+    """
+    Generate objects in an S3 bucket.
+
+    :param bucket: Name of the S3 bucket.
+    :param prefix: Only fetch objects whose key starts with
+        this prefix (optional).
+    :param suffix: Only fetch objects whose keys end with
+        this suffix (optional).
+    """
+    s3 = boto3.client('s3')
+    kwargs = {'Bucket': bucket}
+
+    # If the prefix is a single string (not a tuple of strings), we can
+    # do the filtering directly in the S3 API.
+    if isinstance(prefix, str):
+        kwargs['Prefix'] = prefix
+
+    while True:
+
+        # The S3 API response is a large blob of metadata.
+        # 'Contents' contains information about the listed objects.
+        resp = s3.list_objects_v2(**kwargs)
+
+        try:
+            contents = resp['Contents']
+        except KeyError:
+            return
+
+        for obj in contents:
+            key = obj['Key']
+            print(obj)
+            if key.endswith(suffix):
+                yield obj
+
+        # The S3 API is paginated, returning up to 1000 keys at a time.
+        # Pass the continuation token into the next response, until we
+        # reach the final page (when this field is missing).
+        try:
+            kwargs['ContinuationToken'] = resp['NextContinuationToken']
+        except KeyError:
+            break
+
+
+def get_matching_s3_keys(bucket, prefix='', suffix=''):
+    """
+    Generate the keys in an S3 bucket.
+
+    :param bucket: Name of the S3 bucket.
+    :param prefix: Only fetch keys that start with this prefix (optional).
+    :param suffix: Only fetch keys that end with this suffix (optional).
+    """
+    for obj in get_matching_s3_objects(bucket, prefix, suffix):
+        yield obj['Key']
+
 
 lambda_handler('test','test')
